39fe60103ad92ffabd59dd1d8c02b90106ca96d2
Callisto <olav.kuhnen1@hotmail.com>
2024-01-04 14:56:27 +0100

    Removed extra READ.me

--- tempapp/app/page.tsx
+++ tempapp/app/page.tsx
@@ -122,7 +122,6 @@ export default function Home() {
             startDate={dateRange.startDate}
             endDate={dateRange.endDate}
             onChange={setDateRange}
-            className="w-full px-3 py-2 text-black placeholder-gray-500 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
           />
         </div>
         {/* Buttons for data fetching and loading saved date range */}
@@ -139,7 +138,7 @@ export default function Home() {
               });
             }
           }} className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
-            Load Saved Date Range
+            Load Saved Date
           </button>
         </div>
       </div>
